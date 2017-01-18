4777e99dafed14a8e79e76812baed2fa1f9e23b8
Chiu-Hsiang Hsu <wdv4758h@gmail.com>
2017-01-18 21:33:34 +0800

    chore: update dependencies
    new version of hyper & openssl need some rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
 #[macro_use]
 extern crate clap;              // CLI arguments
-extern crate hyper;             // HTTP/HTTPS client
+extern crate hyper;             // HTTP client
+extern crate hyper_native_tls;  // HTTPS implementation
 extern crate serde;             // JSON
 extern crate serde_json;        // JSON
 extern crate rustc_serialize;   // base64
 extern crate openssl;           // RSA
-extern crate openssl_sys;       // RSA
 
 use clap::App;
 use hyper::client::Client;
+use hyper::net::HttpsConnector;
+use hyper_native_tls::NativeTlsClient;
 use serde_json::Map;
 use rustc_serialize::base64::{ToBase64, STANDARD};
-use openssl::crypto::rsa::RSA;
-use openssl_sys::RSA_public_encrypt;
+use openssl::rsa::{Rsa, PKCS1_PADDING};
 
 use std::io::prelude::*;    // .read_to_string
 
 
 fn get_public_key(repo: &str) -> String {
     let url = format!("https://api.travis-ci.org/repos/{}/key", repo);
-    let client = Client::new();
+
+    let ssl = NativeTlsClient::new().unwrap();
+    let connector = HttpsConnector::new(ssl);
+    let client = Client::with_connector(connector);
+
     let mut result = "".to_string();
-    client.get(&url)
-          .send().unwrap()
-          .read_to_string(&mut result);
+    let _ = client.get(&url)
+                  .send().unwrap()
+                  .read_to_string(&mut result);
     let map: Map<String, String> =
         serde_json::from_str(result.as_str()).unwrap();
     map["key"].clone()
@@ -37,23 +42,16 @@ fn encrypt(repo: &str, data: &str) -> String {
     ////////////////////
 
     let mut public_key = public_key.as_bytes();
-    let rsa = RSA::public_key_from_pem(&mut public_key).unwrap();
+    let rsa = Rsa::public_key_from_pem(&mut public_key).unwrap();
 
     ////////////////////
     // Encryption
     ////////////////////
 
-    // https://www.openssl.org/docs/manmaster/crypto/RSA_public_encrypt.html
-    // https://sfackler.github.io/rust-openssl/doc/v0.7.14/openssl_sys/fn.RSA_public_encrypt.html
-    const RSA_PKCS1_PADDING: i32 = 1;
-    let mut result = vec![0; rsa.size().unwrap() as usize];
-    unsafe {
-        RSA_public_encrypt((&data).len() as i32,
-                           data.as_ptr(),
-                           result.as_mut_ptr(),
-                           rsa.as_ptr(),
-                           RSA_PKCS1_PADDING);
-    }
+    let mut result = vec![0; rsa.size()];
+    let _ = rsa.public_encrypt(data.as_bytes(),
+                               &mut result,
+                               PKCS1_PADDING);
     result.to_base64(STANDARD)
 }
 
@@ -64,7 +62,7 @@ fn main() {
     // Parse Arguments
     ////////////////////
 
-    let yml = load_yaml!("arguments.yml");
+    let yml = load_yaml!("cli.yml");
     let arguments = App::from_yaml(yml).get_matches();
     let repo = arguments.value_of("repo").unwrap();
     let data = arguments.value_of("data").unwrap();
